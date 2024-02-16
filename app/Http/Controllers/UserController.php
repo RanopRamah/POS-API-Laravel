d3af36f4e4f681000845c23dbad8b680336903e8
RanopRamah <101007427+RanopRamah@users.noreply.github.com>
2024-02-16 23:15:46 +0700

    Migrate

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -8,24 +8,28 @@ use Illuminate\Support\Facades\Hash;
 
 class UserController extends Controller
 {
-    public function store(Request $request)
-    {
-        try{
-       $request->validate([
-        'username' => 'required|max:255|string',
-        'password' => 'required|min:8|string'
-       ]);
 
-       User::create([
-        'username' => $request->input('username'),
-        'password' => Hash::make($request->input('password'))
-       ]);
 
-       return response()->json(['message' => 'User created Successfully'], 201);
+public function store(Request $request)
+{
+    try {
+        $request->validate([
+            'username' => 'required|max:255|string',
+            'email' => 'required|email|unique:users,email',
+            'password' => 'required|min:8|string'
+        ]);
+
+        $user = new User();
+        $user->username = $request->input('username');
+        $user->email = $request->input('email'); 
+        $user->password = Hash::make($request->input('password'));
+        $user->save();
+
+        return response()->json(['message' => 'User created Successfully'], 201);
     } catch (\Exception $e) {
-        return response()->json(['message' => 'Failed to create user' . $e->getMessage()], 401);
+        return response()->json(['message' => 'Failed to create user: ' . $e->getMessage()], 401);
     }
-       
-    }
+}
+
     public function show(){}
 }
